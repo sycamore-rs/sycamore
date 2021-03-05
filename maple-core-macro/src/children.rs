37c80f88020386ba88f43e7fa329ac5ab0b1cdd6
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2021-03-05 15:33:52 -0800

    Parse children

---
+++ maple-core-macro/src/children.rs
@@ -0,0 +1,27 @@
+use syn::ext::IdentExt;
+use syn::parse::{Parse, ParseStream};
+use syn::{braced, token, Ident, Result};
+
+use crate::HtmlTree;
+
+pub(crate) struct Children {
+    pub _brace_token: token::Brace,
+    pub body: Vec<HtmlTree>,
+}
+
+impl Parse for Children {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let content;
+        let brace_token = braced!(content in input);
+        let mut body = Vec::new();
+
+        while content.peek(Ident::peek_any) {
+            body.push(content.parse()?);
+        }
+
+        Ok(Self {
+            _brace_token: brace_token,
+            body,
+        })
+    }
+}
--- maple-core-macro/src/element.rs
+++ maple-core-macro/src/element.rs
@@ -3,22 +3,59 @@ use quote::{quote, ToTokens};
 use syn::ext::IdentExt;
 use syn::parse::{Parse, ParseStream, Result};
 use syn::punctuated::Punctuated;
-use syn::{parenthesized, token, Expr, ExprAssign, Ident, Token};
+use syn::{parenthesized, token, Expr, ExprAssign, ExprPath, Ident, Token};
+
+use crate::children::Children;
 
 /// Represents a html element with all its attributes and properties (e.g. `p(class="text")`).
-pub struct HtmlElement {
+pub(crate) struct HtmlElement {
     tag_name: TagName,
     _paren_token: token::Paren,
     attributes: Punctuated<Expr, Token![,]>,
+    children: Option<Children>,
 }
 
 impl Parse for HtmlElement {
     fn parse(input: ParseStream) -> Result<Self> {
+        let tag_name = input.parse()?;
         let attributes;
+        let paren_token = parenthesized!(attributes in input);
+        let attributes = attributes.parse_terminated(Expr::parse)?;
+
+        let children = if input.peek(token::Brace) {
+            Some(input.parse()?)
+        } else {
+            None
+        };
+
+        // check attribute syntax
+        for attribute in &attributes {
+            match attribute {
+                Expr::Assign(ExprAssign {
+                    attrs: _,
+                    left,
+                    eq_token: _,
+                    right: _,
+                }) => {
+                    if !matches!(left.as_ref(), Expr::Path(ExprPath {path, ..}) if path.segments.len() == 1)
+                    {
+                        return Err(syn::Error::new_spanned(left, "expected an identifier"));
+                    }
+                }
+                _ => {
+                    return Err(syn::Error::new_spanned(
+                        attribute,
+                        "expected an assignment expression",
+                    ))
+                }
+            }
+        }
+
         Ok(Self {
-            tag_name: input.parse()?,
-            _paren_token: parenthesized!(attributes in input),
-            attributes: attributes.parse_terminated(Expr::parse)?,
+            tag_name,
+            _paren_token: paren_token,
+            attributes,
+            children,
         })
     }
 }
@@ -29,32 +66,41 @@ impl ToTokens for HtmlElement {
             tag_name,
             _paren_token: _,
             attributes,
+            children,
         } = self;
 
         let mut set_attributes = Vec::new();
         for attribute in attributes {
             match attribute {
-                Expr::Assign(expr) => {
-                    let ExprAssign {
-                        attrs: _,
-                        left,
-                        eq_token: _,
-                        right,
-                    } = expr;
-
+                Expr::Assign(ExprAssign {
+                    attrs: _,
+                    left,
+                    eq_token: _,
+                    right,
+                }) => {
                     let left_str = left.to_token_stream().to_string();
 
                     set_attributes.push(quote! {
                         ::maple_core::internal::attr(&element, #left_str, #right);
                     });
                 }
-                _ => unreachable!(),
+                _ => unreachable!("attribute syntax checked during parsing"),
+            }
+        }
+
+        let mut append_children = Vec::new();
+        if let Some(children) = children {
+            for child in &children.body {
+                append_children.push(quote! {
+                    ::maple_core::internal::append(&element, #child);
+                });
             }
         }
 
         let quoted = quote! {{
             let element = #tag_name;
-            #(#set_attributes);*
+            #(#set_attributes)*
+            #(#append_children)*
             element
         }};
         tokens.extend(quoted);
