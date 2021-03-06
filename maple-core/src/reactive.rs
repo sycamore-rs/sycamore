a06b8f5269d387484a5f3cfb5111783a9767d417
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2021-03-06 14:12:10 -0800

    Effects

--- maple-core/src/reactive.rs
+++ maple-core/src/reactive.rs
@@ -4,47 +4,67 @@ use std::cell::RefCell;
 use std::rc::Rc;
 
 pub struct Signal<T> {
-    value: Rc<T>,
-    observers: Vec<Computation<T>>,
+    inner: Rc<T>,
+    observers: Vec<Rc<RefCell<Computation>>>,
 }
 
 impl<T> Signal<T> {
     fn new(value: T) -> Self {
         Self {
-            value: Rc::new(value),
+            inner: Rc::new(value),
             observers: Vec::new(),
         }
     }
 
-    fn observe(&mut self, handler: Box<dyn FnMut(Rc<T>)>) {
-        let mut computation = Computation(handler);
-        computation.0(self.value.clone()); // call computation when added
-
-        self.observers.push(computation);
+    fn observe(&mut self, handler: Rc<RefCell<Computation>>) {
+        self.observers.push(handler);
     }
 
     fn update(&mut self, new_value: T) {
-        self.value = Rc::new(new_value);
+        self.inner = Rc::new(new_value);
+    }
 
-        // call all observers
-        for observer in &mut self.observers {
-            observer.0(self.value.clone());
+    fn trigger_observers(&self) {
+        for observer in &self.observers {
+            observer.borrow_mut().0();
         }
     }
 }
 
 /// A derived computation from a signal. Takes the new value as an input.
-pub struct Computation<T>(Box<dyn FnMut(Rc<T>)>);
+pub struct Computation(Box<dyn FnMut()>);
 
-pub fn create_signal<T: 'static>(value: T) -> (impl Fn() -> Rc<T>, impl FnMut(T)) {
+thread_local! {
+    static HANDLER: RefCell<Option<Rc<RefCell<Computation>>>> = RefCell::new(None);
+
+    /// To add the dependencies, iterate through functions and execute them.
+    static DEPENDENCIES: RefCell<Option<Vec<Box<dyn Fn()>>>> = RefCell::new(None);
+}
+
+pub fn create_signal<T: 'static>(value: T) -> (impl Fn() -> Rc<T>, impl Fn(T)) {
     let signal = Rc::new(RefCell::new(Signal::new(value)));
 
     let getter = {
         let signal = signal.clone();
         move || {
-            // TODO: if inside an effect, add this signal to dependency list
+            // if inside an effect, add this signal to dependency list
+            DEPENDENCIES.with(|dependencies| {
+                if dependencies.borrow().is_some() {
+                    let signal = signal.clone();
+                    let handler =
+                        HANDLER.with(|handler| handler.borrow().as_ref().unwrap().clone());
 
-            signal.borrow().value.clone()
+                    dependencies
+                        .borrow_mut()
+                        .as_mut()
+                        .unwrap()
+                        .push(Box::new(move || {
+                            signal.borrow_mut().observe(handler.clone())
+                        }));
+                }
+            });
+
+            signal.borrow().inner.clone()
         }
     };
 
@@ -52,13 +72,71 @@ pub fn create_signal<T: 'static>(value: T) -> (impl Fn() -> Rc<T>, impl FnMut(T)
         let signal = signal.clone();
         move |new_value| {
             signal.borrow_mut().update(new_value);
+            signal.borrow().trigger_observers();
         }
     };
 
     (getter, setter)
 }
 
+pub fn create_effect<F>(effect: F)
+where
+    F: FnMut() + 'static,
+{
+    DEPENDENCIES.with(|dependencies| {
+        if dependencies.borrow().is_some() {
+            unimplemented!("nested effects are not supported")
+        }
+
+        let effect = Rc::new(RefCell::new(Computation(Box::new(effect))));
+
+        *dependencies.borrow_mut() = Some(Vec::new());
+        HANDLER.with(|handler| *handler.borrow_mut() = Some(effect.clone()));
+
+        // run effect for the first time to attach all the dependencies
+        effect.borrow_mut().0();
+
+        // attach dependencies
+        for dependency in dependencies.borrow().as_ref().unwrap() {
+            dependency();
+        }
+
+        // Reset dependencies for next effect hook
+        *dependencies.borrow_mut() = None;
+    })
+}
+
 #[cfg(test)]
 mod tests {
+    use std::cell::Cell;
+
     use super::*;
+
+    #[test]
+    fn signals() {
+        let (state, set_state) = create_signal(0);
+        assert_eq!(*state(), 0);
+
+        set_state(1);
+        assert_eq!(*state(), 1);
+    }
+
+    #[test]
+    fn effects() {
+        let (state, set_state) = create_signal(0);
+        assert_eq!(*state(), 0);
+
+        let double = Rc::new(Cell::new(0));
+        create_effect({
+            let double = double.clone();
+            move || {
+                double.set(*state() * 2);
+            }
+        });
+
+        set_state(1);
+        assert_eq!(double.get(), 2);
+        set_state(2);
+        assert_eq!(double.get(), 4);
+    }
 }
