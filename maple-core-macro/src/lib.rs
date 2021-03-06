8fe23cb3b14ad42b3f5e67a2797457fdf038e5d9
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2021-03-06 09:39:53 -0800

    Make template! return a closure

--- maple-core-macro/src/lib.rs
+++ maple-core-macro/src/lib.rs
@@ -3,7 +3,7 @@ mod element;
 mod text;
 
 use proc_macro::TokenStream;
-use quote::ToTokens;
+use quote::{quote, ToTokens};
 use syn::parse::{Parse, ParseStream};
 use syn::{parse_macro_input, Result, Token};
 
@@ -35,5 +35,9 @@ impl ToTokens for HtmlTree {
 pub fn template(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as element::HtmlElement);
 
-    TokenStream::from(input.to_token_stream())
+    let quoted = quote! {
+        || { #input }
+    };
+
+    TokenStream::from(quoted)
 }
