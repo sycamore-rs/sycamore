e7948aa2a6cb0163fbac6dd48ab982903093b18a
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2021-03-06 14:59:50 -0800

    Create memo (derived) values

--- maple-core/src/reactive.rs
+++ maple-core/src/reactive.rs
@@ -5,7 +5,7 @@ use std::rc::Rc;
 
 pub struct Signal<T> {
     inner: Rc<T>,
-    observers: Vec<Rc<RefCell<Computation>>>,
+    observers: Vec<Rc<Computation>>,
 }
 
 impl<T> Signal<T> {
@@ -16,7 +16,7 @@ impl<T> Signal<T> {
         }
     }
 
-    fn observe(&mut self, handler: Rc<RefCell<Computation>>) {
+    fn observe(&mut self, handler: Rc<Computation>) {
         self.observers.push(handler);
     }
 
@@ -26,22 +26,22 @@ impl<T> Signal<T> {
 
     fn trigger_observers(&self) {
         for observer in &self.observers {
-            observer.borrow_mut().0();
+            observer.0();
         }
     }
 }
 
 /// A derived computation from a signal. Takes the new value as an input.
-pub struct Computation(Box<dyn FnMut()>);
+pub struct Computation(Box<dyn Fn()>);
 
 thread_local! {
-    static HANDLER: RefCell<Option<Rc<RefCell<Computation>>>> = RefCell::new(None);
+    static HANDLER: RefCell<Option<Rc<Computation>>> = RefCell::new(None);
 
     /// To add the dependencies, iterate through functions and execute them.
     static DEPENDENCIES: RefCell<Option<Vec<Box<dyn Fn()>>>> = RefCell::new(None);
 }
 
-pub fn create_signal<T: 'static>(value: T) -> (impl Fn() -> Rc<T>, impl Fn(T)) {
+pub fn create_signal<T: 'static>(value: T) -> (Rc<impl Fn() -> Rc<T>>, Rc<impl Fn(T)>) {
     let signal = Rc::new(RefCell::new(Signal::new(value)));
 
     let getter = {
@@ -71,30 +71,34 @@ pub fn create_signal<T: 'static>(value: T) -> (impl Fn() -> Rc<T>, impl Fn(T)) {
     let setter = {
         let signal = signal.clone();
         move |new_value| {
-            signal.borrow_mut().update(new_value);
+            match signal.try_borrow_mut() {
+                Ok(mut signal) => signal.update(new_value),
+                // If the signal is already borrowed, that means it is borrowed in the getter, thus creating a cyclic dependency.
+                Err(_err) => panic!("cannot create cyclic dependency"),
+            };
             signal.borrow().trigger_observers();
         }
     };
 
-    (getter, setter)
+    (Rc::new(getter), Rc::new(setter))
 }
 
 pub fn create_effect<F>(effect: F)
 where
-    F: FnMut() + 'static,
+    F: Fn() + 'static,
 {
     DEPENDENCIES.with(|dependencies| {
         if dependencies.borrow().is_some() {
-            unimplemented!("nested effects are not supported")
+            unimplemented!("nested dependencies are not supported")
         }
 
-        let effect = Rc::new(RefCell::new(Computation(Box::new(effect))));
+        let effect = Rc::new(Computation(Box::new(effect)));
 
         *dependencies.borrow_mut() = Some(Vec::new());
         HANDLER.with(|handler| *handler.borrow_mut() = Some(effect.clone()));
 
         // run effect for the first time to attach all the dependencies
-        effect.borrow_mut().0();
+        effect.0();
 
         // attach dependencies
         for dependency in dependencies.borrow().as_ref().unwrap() {
@@ -104,6 +108,26 @@ where
         // Reset dependencies for next effect hook
         *dependencies.borrow_mut() = None;
     })
+}
+
+pub fn create_memo<'out, F, Out>(derived: F) -> impl Fn() -> Rc<Out>
+where
+    F: Fn() -> Out + 'static,
+    Out: 'static,
+{
+    let derived = Rc::new(derived);
+    let memo = Rc::new(RefCell::new(None));
+
+    create_effect({
+        let derived = derived.clone();
+        let memo = memo.clone();
+        move || {
+            *memo.borrow_mut() = Some(Rc::new(derived()));
+        }
+    });
+
+    // return memoized result
+    move || memo.borrow().as_ref().unwrap().clone()
 }
 
 #[cfg(test)]
@@ -124,19 +148,59 @@ mod tests {
     #[test]
     fn effects() {
         let (state, set_state) = create_signal(0);
-        assert_eq!(*state(), 0);
 
-        let double = Rc::new(Cell::new(0));
+        let (double, set_double) = create_signal(-1);
+
         create_effect({
-            let double = double.clone();
+            let set_double = set_double.clone();
             move || {
-                double.set(*state() * 2);
+                set_double(*state() * 2);
             }
         });
+        assert_eq!(*double(), 0); // calling create_effect should call the effect at least once
 
         set_state(1);
-        assert_eq!(double.get(), 2);
+        assert_eq!(*double(), 2);
         set_state(2);
-        assert_eq!(double.get(), 4);
+        assert_eq!(*double(), 4);
+    }
+
+    #[test]
+    fn memo() {
+        let (state, set_state) = create_signal(0);
+
+        let double = create_memo(move || *state() * 2);
+        assert_eq!(*double(), 0);
+
+        set_state(1);
+        assert_eq!(*double(), 2);
+
+        set_state(2);
+        assert_eq!(*double(), 4);
+    }
+
+    #[test]
+    /// Make sure value is memoized rather than executed on demand.
+    fn memo_only_run_once() {
+        let (state, set_state) = create_signal(0);
+
+        // use a Cell instead of a signal to prevent circular dependencies
+        // TODO: change to create_signal once explicit tracking is implemented
+        let counter = Rc::new(Cell::new(0));
+
+        let double = create_memo({
+            let counter = counter.clone();
+            move || {
+                counter.set(counter.get() + 1);
+
+                *state() * 2
+            }
+        });
+        assert_eq!(counter.get(), 1); // once for calculating initial derived state
+
+        set_state(2);
+        assert_eq!(counter.get(), 2);
+        assert_eq!(*double(), 4);
+        assert_eq!(counter.get(), 2); // should still be 2 after access
     }
 }
