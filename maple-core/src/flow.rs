9b36e6c823aa1e95554bd04f7e3e12a80fa970b9
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2021-03-23 20:26:17 -0700

    Keyed

--- maple-core/src/flow.rs
+++ maple-core/src/flow.rs
@@ -1,7 +1,7 @@
 //! Keyed iteration in [`template`](crate::template).
 
 use std::cell::RefCell;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::hash::Hash;
 use std::mem;
 use std::rc::Rc;
@@ -29,50 +29,12 @@ pub fn Keyed<T, F: 'static, K: 'static, Key: 'static>(
 where
     F: Fn(T) -> TemplateResult,
     K: Fn(&T) -> Key,
-    Key: Hash + Eq,
-    T: Clone,
-{
-    let map = RefCell::new(HashMap::new());
-
-    let _old_keys: Vec<_> = props
-        .iterable
-        .get()
-        .iter()
-        .map(|item| (props.key)(item))
-        .collect();
-
-    create_effect(move || {
-        for item in props.iterable.get().iter() {
-            let key = (props.key)(item);
-
-            if !map.borrow().contains_key(&key) {
-                map.borrow_mut().insert(key, (props.template)(item.clone()));
-            }
-        }
-    });
-
-    todo!();
-}
-
-pub struct IndexedProps<T: 'static, F>
-where
-    F: Fn(T) -> TemplateResult,
-{
-    pub iterable: Signal<Vec<T>>,
-    pub template: F,
-}
-
-pub fn Indexed<T, F: 'static>(props: IndexedProps<T, F>) -> TemplateResult
-where
+    Key: Clone + Hash + Eq,
     T: Clone + PartialEq,
-    F: Fn(T) -> TemplateResult,
 {
-    let templates: Rc<RefCell<Vec<Option<TemplateResult>>>> = Rc::new(RefCell::new(Vec::new()));
-
-    // Previous values for diffing purposes.
-    let previous_values = RefCell::new(Vec::new());
-    // A list of indexes that need to be re-rendered.
-    let changed = Rc::new(RefCell::new(Vec::new()));
+    // A tuple with a value of type `T` and the `TemplateResult` produces by calling `props.template` with the first value.
+    let templates: Rc<RefCell<HashMap<Key, (T, Option<TemplateResult>)>>> =
+        Rc::new(RefCell::new(HashMap::new()));
 
     let fragment = web_sys::window()
         .unwrap()
@@ -92,17 +54,141 @@ where
         let templates = Rc::clone(&templates);
         let marker = marker.clone();
         move || {
-            *changed.borrow_mut() = Vec::new(); // reset changed
+            let previous_values = (*templates.borrow()).clone();
 
-            let mut changed_tmp = Vec::new();
+            // Find values that changed by comparing to previous_values.
+            for (i, item) in props.iterable.get().iter().enumerate() {
+                let key = (props.key)(item);
 
+                let previous_value = previous_values.get(&key);
+
+                if previous_value.is_none() || &previous_value.unwrap().0 != item {
+                    // value changed, re-render item
+
+                    if templates.borrow().get(&key).is_some() {
+                        let new_node = (props.template)(item.clone());
+                        let (_, old_node) = mem::replace(
+                            templates.borrow_mut().get_mut(&key).unwrap(),
+                            (item.clone(), Some(new_node.clone())),
+                        );
+
+                        let parent = old_node.as_ref().unwrap().node.parent_node().unwrap();
+                        parent
+                            .replace_child(&new_node.node, &old_node.unwrap().node)
+                            .unwrap();
+                    } else {
+                        debug_assert!(templates.borrow().len() == i, "pushing new value scenario");
+
+                        templates.borrow_mut().insert(
+                            key.clone(),
+                            (item.clone(), Some((props.template)(item.clone()))),
+                        );
+
+                        marker
+                            .before_with_node_1(
+                                &templates
+                                    .borrow()
+                                    .get(&key)
+                                    .as_ref()
+                                    .unwrap()
+                                    .1
+                                    .as_ref()
+                                    .unwrap()
+                                    .node,
+                            )
+                            .unwrap();
+                    }
+                }
+            }
+
+            if templates.borrow().len() > props.iterable.get().len() {
+                let mut templates = templates.borrow_mut();
+                let new_keys: HashSet<Key> = props
+                    .iterable
+                    .get()
+                    .iter()
+                    .map(|item| (props.key)(item))
+                    .collect();
+
+                let excess_nodes = templates
+                    .iter()
+                    .filter(|item| new_keys.get(item.0).is_none())
+                    .map(|x| (x.0.clone(), x.1.clone()))
+                    .collect::<Vec<_>>();
+
+                for node in &excess_nodes {
+                    templates.remove(&node.0);
+                }
+
+                for node in excess_nodes {
+                    node.1
+                         .1
+                        .as_ref()
+                        .unwrap()
+                        .clone()
+                        .node
+                        .unchecked_into::<Element>()
+                        .remove();
+                }
+            }
+
+            debug_assert!(
+                templates.borrow().values().all(|item| item.1.is_some()),
+                "templates should all be Some"
+            );
+        }
+    });
+
+    for template in templates.borrow().values() {
+        let template = template.1.as_ref().unwrap().clone();
+        marker.before_with_node_1(&template.node).unwrap();
+    }
+
+    TemplateResult::new(fragment.into())
+}
+
+pub struct IndexedProps<T: 'static, F>
+where
+    F: Fn(T) -> TemplateResult,
+{
+    pub iterable: Signal<Vec<T>>,
+    pub template: F,
+}
+
+pub fn Indexed<T, F: 'static>(props: IndexedProps<T, F>) -> TemplateResult
+where
+    T: Clone + PartialEq,
+    F: Fn(T) -> TemplateResult,
+{
+    let templates: Rc<RefCell<Vec<Option<TemplateResult>>>> = Rc::new(RefCell::new(Vec::new()));
+
+    // Previous values for diffing purposes.
+    let previous_values = RefCell::new(Vec::new());
+
+    let fragment = web_sys::window()
+        .unwrap()
+        .document()
+        .unwrap()
+        .create_document_fragment();
+
+    let marker = web_sys::window()
+        .unwrap()
+        .document()
+        .unwrap()
+        .create_comment("");
+
+    append(&fragment, &marker);
+
+    create_effect({
+        let templates = Rc::clone(&templates);
+        let marker = marker.clone();
+        move || {
             // Find values that changed by comparing to previous_values.
             for (i, item) in props.iterable.get().iter().enumerate() {
                 let previous_values = previous_values.borrow();
                 let previous_value = previous_values.get(i);
 
                 if previous_value.is_none() || previous_value.unwrap() != item {
-                    changed_tmp.push(i);
                     // value changed, re-render item
 
                     if templates.borrow().get(i).is_some() {
@@ -140,10 +226,8 @@ where
 
             *previous_values.borrow_mut() = (*props.iterable.get()).clone();
 
-            *changed.borrow_mut() = changed_tmp;
-
             debug_assert!(
-                templates.borrow_mut().iter().all(|item| item.is_some()),
+                templates.borrow().iter().all(|item| item.is_some()),
                 "templates should all be Some"
             );
         }
