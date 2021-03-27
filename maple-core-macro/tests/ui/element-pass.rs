4339f69529f9d917eceed7439d72886feee531b0
lights0123 <developer@lights0123.com>
2021-03-27 11:46:36 -0400

    Fix tests

--- maple-core-macro/tests/ui/element-pass.rs
+++ maple-core-macro/tests/ui/element-pass.rs
@@ -1,17 +1,17 @@
 use maple_core::prelude::*;
 
-fn compile_pass() {
-    template! { p };
-    template! { custom-element };
+fn compile_pass<G: GenericNode>() {
+    let _: TemplateResult<G> = template! { p };
+    let _: TemplateResult<G> = template! { custom-element };
 
-    template! { p() };
-    template! { custom-element() };
+    let _: TemplateResult<G> = template! { p() };
+    let _: TemplateResult<G> = template! { custom-element() };
 
-    template! { p(class="my-class") };
-    template! { p(class="my-class", id="my-id") };
+    let _: TemplateResult<G> = template! { p(class="my-class") };
+    let _: TemplateResult<G> = template! { p(class="my-class", id="my-id") };
 
-    template! { button(class="my-btn", on:click=|_| {}) };
-    template! { button(class="my-btn", aria-hidden="true") };
+    let _: TemplateResult<G> = template! { button(class="my-btn", on:click=|_| {}) };
+    let _: TemplateResult<G> = template! { button(class="my-btn", aria-hidden="true") };
 }
 
 fn main() {}
