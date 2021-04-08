228dbe5bb15d1f434756603daa1c56dc95248aaf
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2021-04-08 08:50:56 -0700

    Remove reqwest from docs

--- docs/src/content.rs
+++ docs/src/content.rs
@@ -3,9 +3,12 @@ use pulldown_cmark::{html, Options, Parser};
 use wasm_bindgen::prelude::*;
 use web_sys::HtmlElement;
 
-#[wasm_bindgen(inline_js = "export function highlight_all() { hljs.highlightAll(); }")]
+#[wasm_bindgen(inline_js = "\
+export function highlight_all() { hljs.highlightAll(); }\
+export async function fetch_md(url) { return await (await fetch(url)).text(); }")]
 extern "C" {
     fn highlight_all();
+    async fn fetch_md(url: &str) -> JsValue;
 }
 
 #[component(Content<G>)]
@@ -45,14 +48,8 @@ pub fn content() -> TemplateResult<G> {
         log::info!("Getting documentation at {}", pathname);
 
         let url = format!("{}/markdown{}.md", location.origin().unwrap(), pathname);
-        match reqwest::get(url).await {
-            Ok(res) => {
-                markdown.set(res.text().await.unwrap());
-            }
-            Err(err) => {
-                log::error!("Unknown error: {}", err);
-            }
-        }
+        let text = fetch_md(&url).await.as_string().unwrap();
+        markdown.set(text);
     }));
 
     template! {
