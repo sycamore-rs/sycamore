ba4c91e4bbb9e27d0c8f3acf4981fd947f1354ec
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2021-04-24 14:52:48 -0700

    Implement map_keyed

--- maple-core/src/reactive/iter.rs
+++ maple-core/src/reactive/iter.rs
@@ -1,11 +1,102 @@
 use std::cell::RefCell;
+use std::collections::HashMap;
+use std::hash::Hash;
 use std::mem;
 use std::rc::Rc;
 
 use super::*;
 
-pub fn map_keyed<T, U>(list: StateHandle<Vec<T>>, map_fn: impl Fn(T) -> U) -> StateHandle<Vec<U>> {
-    todo!();
+// Credits: Ported from TypeScript implementation in https://github.com/ryansolid/solid
+pub fn map_keyed<T, U>(
+    list: StateHandle<Vec<T>>,
+    map_fn: impl Fn(&T) -> U + 'static,
+) -> Rc<RefCell<Vec<U>>>
+where
+    T: Eq + Clone + Hash,
+    U: Clone + 'static,
+{
+    // Previous state used for diffing.
+    let mut items = Vec::new();
+    let mapped = Rc::new(RefCell::new(Vec::<U>::new()));
+    let mut scopes: Vec<Option<Rc<ReactiveScope>>> = Vec::new();
+
+    create_effect({
+        let mapped = Rc::clone(&mapped);
+        move || {
+            let new_items = list.get();
+            untrack(|| {
+                // Skip common prefix.
+                let mut start = 0;
+                let end = usize::min(items.len(), new_items.len());
+                while start < end && items[start] == new_items[start] {
+                    start += 1;
+                }
+
+                // Skip common suffix.
+                let mut end = items.len() as isize - 1;
+                let mut new_end = new_items.len() - 1;
+                #[allow(clippy::suspicious_operation_groupings)]
+                // FIXME: make code clearer so that clippy won't complain
+                while (start as isize) < end
+                    && start < new_end
+                    && items[end as usize] == new_items[new_end]
+                {
+                    end -= 1;
+                    new_end -= 1;
+                }
+
+                // Prepare a map of indices in newItems. Scan backwards so we encounter them in natural order.
+                let mut new_indices = HashMap::new();
+                for i in (start..=new_end).rev() {
+                    let item = &new_items[i];
+                    new_indices.insert(item, i);
+                }
+
+                // Step through old items and see if they can be found in new set; if so, mark them as moved.
+                let mut temp = HashMap::new();
+                let mut temp_scopes = HashMap::new();
+                if (start as isize) < end {
+                    for i in start..=end as usize {
+                        let item = &items[i];
+                        if let Some(j) = new_indices.get(item).copied() {
+                            temp.insert(j, mapped.borrow()[i].clone());
+                            temp_scopes.insert(j, scopes.get(i).unwrap().as_ref().cloned());
+                        } else {
+                            scopes[i] = None;
+                        }
+                    }
+                }
+
+                // Set all the new values, pulling from the temp array if copied, otherwise entering the new value.
+                for i in start..new_items.len() {
+                    if temp.get(&i).is_some() {
+                        mapped.borrow_mut()[i] = temp.remove(&i).unwrap();
+                        scopes[i] = temp_scopes.remove(&i).unwrap();
+                    } else {
+                        let mut new_mapped = None;
+                        let new_scope = create_root(|| {
+                            new_mapped = Some(map_fn(&new_items[i]));
+                        });
+
+                        if mapped.borrow().len() > i {
+                            mapped.borrow_mut()[i] = new_mapped.unwrap();
+                            scopes[i] = Some(Rc::new(new_scope));
+                        } else {
+                            mapped.borrow_mut().push(new_mapped.unwrap());
+                            scopes.push(Some(Rc::new(new_scope)));
+                        }
+                    }
+                }
+
+                items = (*new_items).clone();
+                debug_assert!([items.len(), mapped.borrow().len(), scopes.len()]
+                    .iter()
+                    .all(|l| *l == new_items.len()));
+            })
+        }
+    });
+
+    mapped
 }
 
 pub fn map_indexed<T, U>(
@@ -17,9 +108,9 @@ where
     U: 'static,
 {
     // Previous state used for diffing.
-    let mut items: Vec<T> = Vec::new();
+    let mut items = Vec::new();
     let mapped = Rc::new(RefCell::new(Vec::new()));
-    let mut scopes: Vec<ReactiveScope> = Vec::new();
+    let mut scopes = Vec::new();
 
     create_effect({
         let mapped = Rc::clone(&mapped);
@@ -73,6 +164,19 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn keyed() {
+        let a = Signal::new(vec![1, 2, 3]);
+        let mapped = map_keyed(a.handle(), |x| *x * 2);
+        debug_assert_eq!(*mapped.borrow(), vec![2, 4, 6]);
+
+        a.set(vec![1, 2, 3, 4]);
+        debug_assert_eq!(*mapped.borrow(), vec![2, 4, 6, 8]);
+
+        a.set(vec![2, 2, 3, 4]);
+        debug_assert_eq!(*mapped.borrow(), vec![4, 4, 6, 8]);
+    }
 
     #[test]
     fn indexed() {
