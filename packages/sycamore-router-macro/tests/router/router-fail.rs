370f7b08e520ac097871a4f37df65adf86248de1
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2021-06-17 08:47:37 -0700

    Add more trybuild tests

--- packages/sycamore-router-macro/tests/router/router-fail.rs
+++ packages/sycamore-router-macro/tests/router/router-fail.rs
@@ -21,4 +21,36 @@ enum Router4 {
     NotFound,
 }
 
+#[derive(Router)]
+enum Router5 {
+    #[to("<capture>")]
+    Path {}, // Missing capture field
+    #[not_found]
+    NotFound,
+}
+
+#[derive(Router)]
+enum Router6 {
+    #[to("<capture>")]
+    Path { not_capture: u32 }, // Wrong capture field name
+    #[not_found]
+    NotFound,
+}
+
+#[derive(Router)]
+enum Router7 {
+    #[to("<a>/<b>")]
+    Path { b: u32, a: u32 }, // Wrong order
+    #[not_found]
+    NotFound,
+}
+
+#[derive(Router)]
+enum Router8 {
+    #[to("<a/b>")] // `a/b` is not an identifier
+    Path { a: u32 },
+    #[not_found]
+    NotFound,
+}
+
 fn main() {}
