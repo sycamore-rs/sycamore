3a5d1ee408c2d6a0daf4b8393bb163ccf1e32a25
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2021-07-08 11:04:51 -0700

    Set className directly instead of calling setAttribute

--- packages/sycamore/src/generic_node.rs
+++ packages/sycamore/src/generic_node.rs
@@ -58,6 +58,13 @@ pub trait GenericNode: fmt::Debug + Clone + PartialEq + Eq + Hash + 'static {
     /// Sets an attribute on a node.
     fn set_attribute(&self, name: &str, value: &str);
 
+    /// Sets the `class` attribute on a node.
+    /// This should have the same outcome as calling `set_attribute("class", value)`.
+    /// For [`DomNode`], this sets the `className` property directly which is about 2x faster (on Chrome).
+    fn set_class_name(&self, value: &str) {
+        self.set_attribute("class", value);
+    }
+
     /// Sets a property on a node.
     fn set_property(&self, name: &str, value: &JsValue);
 
