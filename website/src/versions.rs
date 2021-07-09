2f246c40291cefc7f6c0110fd2fcc4cfbe84b04d
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2021-07-09 11:13:15 -0700

    v0.5.1

--- website/src/versions.rs
+++ website/src/versions.rs
@@ -8,7 +8,8 @@ enum VersionedDocsLink {
 
 const VERSIONS: &[(&str, VersionedDocsLink)] = &[
     ("Next", VersionedDocsLink::Next),
-    ("v0.5.0", VersionedDocsLink::Some("v0.5")),
+    ("v0.5.1", VersionedDocsLink::Some("v0.5")),
+    ("v0.5.0", VersionedDocsLink::None),
     ("v0.5.0-beta.1", VersionedDocsLink::None),
     ("v0.5.0-beta.0", VersionedDocsLink::None),
 ];
