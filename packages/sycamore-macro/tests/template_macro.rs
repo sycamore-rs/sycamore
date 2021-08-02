207812312949ab51a108aa4b3b02ff0fa302e163
Tshepang Lekhonkhobe <tshepang@gmail.com>
2021-08-02 20:52:09 +0200

    set MSRV (to 1.53), and test on macOS and Windows
    Also, run UI test only on a single compiler version,
    because rustc error messages tend to vary between releases.
    
    Closes #191

--- packages/sycamore-macro/tests/template_macro.rs
+++ packages/sycamore-macro/tests/template_macro.rs
@@ -1,7 +1,8 @@
 #[test]
 fn ui() {
     let t = trybuild::TestCases::new();
-    t.compile_fail("tests/template/*-fail.rs");
-
     t.pass("tests/template/*-pass.rs");
+    if std::env::var("RUN_UI_TESTS").is_ok() {
+        t.compile_fail("tests/template/*-fail.rs");
+    }
 }
--- packages/sycamore-router-macro/tests/router_macros.rs
+++ packages/sycamore-router-macro/tests/router_macros.rs
@@ -1,7 +1,8 @@
 #[test]
 fn ui() {
     let t = trybuild::TestCases::new();
-    t.compile_fail("tests/router/*-fail.rs");
-
     t.pass("tests/router/*-pass.rs");
+    if std::env::var("RUN_UI_TESTS").is_ok() {
+        t.compile_fail("tests/router/*-fail.rs");
+    }
 }
