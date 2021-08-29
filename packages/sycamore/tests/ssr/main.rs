728fb09eeaebac591cd672a6dc4904ee2d2a07d5
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2021-08-29 13:43:53 -0700

    Fix SsrNode remove_child

--- packages/sycamore/tests/ssr/main.rs
+++ packages/sycamore/tests/ssr/main.rs
@@ -71,3 +71,30 @@ fn fragments() {
         "<p>1</p><p>2</p><p>3</p>"
     );
 }
+
+#[test]
+fn indexed() {
+    let count = Signal::new(vec![1, 2]);
+
+    let node = cloned!((count) => template! {
+        ul {
+            Indexed(IndexedProps {
+                iterable: count.handle(),
+                template: |item| template! {
+                    li { (item) }
+                },
+            })
+        }
+    });
+
+    let actual = sycamore::render_to_string(|| node.clone());
+    assert_eq!(actual, "<ul><li>1</li><li>2</li></ul>");
+
+    count.set(count.get().iter().cloned().chain(Some(3)).collect());
+    let actual = sycamore::render_to_string(|| node.clone());
+    assert_eq!(actual, "<ul><li>1</li><li>2</li><li>3</li></ul>");
+
+    count.set(count.get()[1..].into());
+    let actual = sycamore::render_to_string(|| node.clone());
+    assert_eq!(actual, "<ul><li>2</li><li>3</li></ul>");
+}
