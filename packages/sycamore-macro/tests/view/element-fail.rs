bd68b333713f2db500fd8180f1208317815844b3
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2021-11-06 15:04:27 -0700

    Fix trybuild ui tests

--- packages/sycamore-macro/tests/view/element-fail.rs
+++ packages/sycamore-macro/tests/view/element-fail.rs
@@ -1,15 +1,15 @@
 use sycamore::prelude::*;
 
 fn compile_fail<G: GenericNode>() {
-    let _: Template<G> = view! { p.my-class#id };
+    let _: View<G> = view! { p.my-class#id };
 
-    let _: Template<G> = view! { button(disabled) };
-    let _: Template<G> = view! { button(on:click) };
-    let _: Template<G> = view! { button(unknown:directive="123") };
+    let _: View<G> = view! { button(disabled) };
+    let _: View<G> = view! { button(on:click) };
+    let _: View<G> = view! { button(unknown:directive="123") };
 
-    let _: Template<G> = view! { button(a.b.c="123") };
+    let _: View<G> = view! { button(a.b.c="123") };
 
-    let _: Template<G> = view! {
+    let _: View<G> = view! {
         p(dangerously_set_inner_html="<span>Test</span>") {
             "Error"
         }
