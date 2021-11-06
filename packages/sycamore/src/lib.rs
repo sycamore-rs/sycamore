542462bf9f02ca97697079216073c20dadcccbf4
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2021-11-06 12:32:21 -0700

    Make wasm-bindgen-interning a feature flag

--- packages/sycamore/src/lib.rs
+++ packages/sycamore/src/lib.rs
@@ -5,15 +5,21 @@
 //! This is the API docs for sycamore. If you are looking for the usage docs, checkout the
 //! [Sycamore Book](https://sycamore-rs.netlify.app/docs/getting_started/installation).
 //!
-//! ## Features
+//! ## Feature Flags
 //! - `dom` (_default_) - Enables rendering templates to DOM nodes. Only useful on
 //!   `wasm32-unknown-unknown` target.
+//! - `experimental-builder-agnostic` - Enables the agnostic backend builder API.
+//! - `experimental-builder-html` - Enables the HTML specific backend builder API. Also enables
+//!   `experimental-builder-agnostic`.
 //! - `futures` - Enables wrappers around `wasm-bindgen-futures` to make it easier to extend a
 //!   reactive scope into an `async` function.
 //! - `ssr` - Enables rendering templates to static strings (useful for Server Side Rendering /
 //!   Pre-rendering).
 //! - `serde` - Enables serializing and deserializing `Signal`s and other wrapper types using
 //!   `serde`.
+//! - `wasm-bindgen-interning` (_default_) - Enables interning for `wasm-bindgen` strings. This
+//!   improves performance at a slight cost in binary size. If you want to minimize the size of the
+//!   result `.wasm` binary, you might want to disable this.
 
 #![warn(clippy::clone_on_ref_ptr)]
 #![warn(clippy::rc_buffer)]
