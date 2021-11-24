4807f4a26024325571d2867da245925dfef6cd22
Luke Chu <lukewchu@outlook.com>
2021-11-24 21:17:52 +0000

    Hydrate reactivity

--- packages/sycamore/tests/web/hydrate.rs
+++ packages/sycamore/tests/web/hydrate.rs
@@ -65,7 +65,9 @@ mod multiple_nodes_at_same_depth {
     fn ssr() {
         check(
             &sycamore::render_to_string(v),
-            expect![[r#"<div data-hk="0.0"><p data-hk="0.1">First</p><p data-hk="0.2">Second</p></div>"#]],
+            expect![[
+                r#"<div data-hk="0.0"><p data-hk="0.1">First</p><p data-hk="0.2">Second</p></div>"#
+            ]],
         );
     }
     #[wasm_bindgen_test]
@@ -103,5 +105,46 @@ mod top_level_fragment {
 
         // Hydration should not change inner html.
         assert_eq!(c.inner_html(), html);
+    }
+}
+
+mod dynamic {
+    use super::*;
+    fn v<G: Html>(state: ReadSignal<i32>) -> View<G> {
+        view! { p { (state.get()) } }
+    }
+    #[test]
+    fn ssr() {
+        check(
+            &sycamore::render_to_string(|| v(Signal::new(0).handle())),
+            expect![[r#"<p data-hk="0.0">0</p>"#]],
+        );
+    }
+    #[wasm_bindgen_test]
+    fn test() {
+        let html = sycamore::render_to_string(|| v(Signal::new(0).handle()));
+        let c = test_container();
+        c.clone().set_inner_html(&html);
+
+        let state = Signal::new(0);
+
+        sycamore::hydrate_to(cloned!(state => move || v(state.handle())), &c);
+
+        // Hydration should not change inner html.
+        assert_eq!(c.inner_html(), html);
+
+        // Reactivity should work normally.
+        state.set(1);
+        assert_eq!(c.query_selector("p").unwrap().unwrap().inner_html(), "1");
+
+        // P tag should still be the SSR-ed node, not a new node.
+        assert_eq!(
+            c.query_selector("p")
+                .unwrap()
+                .unwrap()
+                .get_attribute("data-hk")
+                .as_deref(),
+            Some("0.0")
+        );
     }
 }
