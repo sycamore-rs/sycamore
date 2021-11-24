0e88c0e47902445c1ac84bec5b7723d3db6fa1b4
Luke Chu <lukewchu@outlook.com>
2021-11-24 21:22:13 +0000

    Add hydrate with siblings test

--- packages/sycamore/tests/web/hydrate.rs
+++ packages/sycamore/tests/web/hydrate.rs
@@ -135,7 +135,62 @@ mod dynamic {
 
         // Reactivity should work normally.
         state.set(1);
-        assert_eq!(c.query_selector("p").unwrap().unwrap().inner_html(), "1");
+        assert_eq!(
+            c.query_selector("p")
+                .unwrap()
+                .unwrap()
+                .text_content()
+                .unwrap(),
+            "1"
+        );
+
+        // P tag should still be the SSR-ed node, not a new node.
+        assert_eq!(
+            c.query_selector("p")
+                .unwrap()
+                .unwrap()
+                .get_attribute("data-hk")
+                .as_deref(),
+            Some("0.0")
+        );
+    }
+}
+
+mod dynamic_with_siblings {
+    use super::*;
+    fn v<G: Html>(state: ReadSignal<i32>) -> View<G> {
+        view! { p { "Value: " (state.get()) "!" } }
+    }
+    #[test]
+    fn ssr() {
+        check(
+            &sycamore::render_to_string(|| v(Signal::new(0).handle())),
+            expect![[r#"<p data-hk="0.0">Value: 0!</p>"#]],
+        );
+    }
+    #[wasm_bindgen_test]
+    fn test() {
+        let html = sycamore::render_to_string(|| v(Signal::new(0).handle()));
+        let c = test_container();
+        c.clone().set_inner_html(&html);
+
+        let state = Signal::new(0);
+
+        sycamore::hydrate_to(cloned!(state => move || v(state.handle())), &c);
+
+        // Hydration should not change inner html.
+        assert_eq!(c.inner_html(), html);
+
+        // Reactivity should work normally.
+        state.set(1);
+        assert_eq!(
+            c.query_selector("p")
+                .unwrap()
+                .unwrap()
+                .text_content()
+                .unwrap(),
+            "Value: 1!"
+        );
 
         // P tag should still be the SSR-ed node, not a new node.
         assert_eq!(
