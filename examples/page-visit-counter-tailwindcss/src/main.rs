80cfb805efef5de5519f08190be463ad320b2b2f
LilUziCert <attriaayush@yahoo.com>
2021-11-30 10:18:37 +0000

    removing link to tailwind
    Update examples/page-visit-counter-tailwindcss/Trunk.toml
    
    Co-authored-by: Luke Chu <37006668+lukechu10@users.noreply.github.com>
    
    Update examples/page-visit-counter-tailwindcss/src/main.rs
    
    Co-authored-by: Luke Chu <37006668+lukechu10@users.noreply.github.com>
    
    Update examples/page-visit-counter-tailwindcss/src/main.rs
    
    Co-authored-by: Luke Chu <37006668+lukechu10@users.noreply.github.com>

--- examples/page-visit-counter-tailwindcss/src/main.rs
+++ examples/page-visit-counter-tailwindcss/src/main.rs
@@ -1,10 +1,8 @@
-use sycamore::prelude::*;
-
 use anyhow::Result;
 use reqwasm::http::Request;
 use serde::{Deserialize, Serialize};
-
 use sycamore::futures::spawn_local_in_scope;
+use sycamore::prelude::*;
 
 // API that counts visits to the web-page
 const API_BASE_URL: &str = "https://api.countapi.xyz/hit";
@@ -30,9 +28,9 @@ fn render_visits(counter: ReadSignal<u64>) -> View<G> {
             h1(class="text-3xl text-blue-700 mb-2 p-1") { "Page Visit Counter" }
             p(class="text-xl p-2") {
                 "Total visits: "
-                    span(class="text-green-500"){
-                        (counter.get())
-                    }
+                span(class="text-green-500") {
+                    (counter.get())
+                }
                 }
             }
         }
