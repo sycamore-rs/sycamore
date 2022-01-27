12e158cabd5155c2194c65efdf888c88a2bc324a
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2022-01-27 11:57:01 -0800

    Fix trybuild UI tests

--- packages/sycamore-macro/tests/view/element-fail.rs
+++ packages/sycamore-macro/tests/view/element-fail.rs
@@ -2,8 +2,6 @@ use sycamore::prelude::*;
 
 fn compile_fail<G: GenericNode>() {
     create_scope_immediate(|ctx| {
-        let _: View<G> = view! { ctx, p.my-class#id };
-
         let _: View<G> = view! { ctx, button(disabled) };
         let _: View<G> = view! { ctx, button(on:click) };
         let _: View<G> = view! { ctx, button(unknown:directive="123") };
