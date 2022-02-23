30df3f5df09ec32197379d17139f55f85d09f831
David Laban <alsuren@gmail.com>
2022-02-23 11:57:04 +0000

    port builder example to 0.8

--- examples/hello-builder/src/main.rs
+++ examples/hello-builder/src/main.rs
@@ -1,26 +1,25 @@
 use sycamore::builder::html::*;
 use sycamore::prelude::*;
 
-#[component(App<G>)]
-fn app() -> View<G> {
-    let name = Signal::new(String::new());
+#[component]
+fn App<G: Html>(ctx: ScopeRef) -> View<G> {
+    let name = ctx.create_signal(String::new());
 
-    div()
+    div(ctx)
         .child(
-            h1().text("Hello ")
-                .dyn_child(cloned!((name) => move || {
-                    if *create_selector(cloned!((name) => move || !name.get().is_empty())).get() {
-                        span()
-                            .dyn_text(cloned!((name) => move || name.get().to_string()))
-                            .build()
+            h1(ctx)
+                .text("Hello ")
+                .dyn_child(move || {
+                    if *ctx.create_selector(move || !name.get().is_empty()).get() {
+                        span(ctx).dyn_text(move || name.get().to_string()).build()
                     } else {
-                        span().text("World").build()
+                        span(ctx).text("World").build()
                     }
-                }))
+                })
                 .text("!")
                 .build(),
         )
-        .child(input().bind_value(name).build())
+        .child(input(ctx).bind_value(&name).build())
         .build()
 }
 
@@ -28,5 +27,5 @@ fn main() {
     console_error_panic_hook::set_once();
     console_log::init_with_level(log::Level::Debug).unwrap();
 
-    sycamore::render(|| view! { App() });
+    sycamore::render(|ctx| view! {ctx, App() });
 }
