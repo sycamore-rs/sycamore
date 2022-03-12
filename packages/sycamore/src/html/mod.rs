1d5f1646814bafd999ad26f2d3bb7489873e7c91
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2022-03-12 12:50:25 -0800

    Fix docs for <input>

--- packages/sycamore/src/html/mod.rs
+++ packages/sycamore/src/html/mod.rs
@@ -96,7 +96,7 @@ define_elements! {
     i {},
     iframe {},
     img {},
-    /// The `<input>` HTML element is used to create interactive controls for web-based forms in order to accept data from the user; a wide variety of types of input data and control widgets are available, depending on the device and user agent. The <input> element is one of the most powerful and complex in all of HTML due to the sheer number of combinations of input types and attributes.
+    /// The `<input>` HTML element is used to create interactive controls for web-based forms in order to accept data from the user; a wide variety of types of input data and control widgets are available, depending on the device and user agent. The `<input>` element is one of the most powerful and complex in all of HTML due to the sheer number of combinations of input types and attributes.
     input {},
     ins {},
     kbd {},
