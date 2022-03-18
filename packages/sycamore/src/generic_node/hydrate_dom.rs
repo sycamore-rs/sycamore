sycamore-rs/sycamore

commit fd087e059c116671d22579018262ef54b0700981

Add a debug check to hydrate new element (#385)

packages/sycamore/src/generic_node/hydrate_dom.rs
@@ -92,6 +92,12 @@ impl GenericNode for HydrateNode {
     fn element<T: SycamoreElement>() -> Self {
         let el = get_next_element();
         if let Some(el) = el {
+            // If in debug mode, check that the hydrate element has the same tag as the argument.
+            debug_assert_eq!(
+                el.tag_name().to_ascii_lowercase(),
+                tag,
+                "hydration error, mismatched element tag"
+            );
             Self {
                 node: DomNode::from_web_sys(el.into()),
             }

commit d46a1cea5480dadef7753a61e0dfdc12020281a2

Fix compile error introduced by #385

packages/sycamore/src/generic_node/hydrate_dom.rs
@@ -95,7 +95,7 @@ impl GenericNode for HydrateNode {
             // If in debug mode, check that the hydrate element has the same tag as the argument.
             debug_assert_eq!(
                 el.tag_name().to_ascii_lowercase(),
-                tag,
+                T::TAG_NAME,
                 "hydration error, mismatched element tag"
             );
             Self {