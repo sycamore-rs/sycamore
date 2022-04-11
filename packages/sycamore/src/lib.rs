e0f1d2299f2ae8a229f762803ea1c5068ad09fd2
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2022-04-10 17:44:00 -0700

    Add an on_mount wrapper around queueMicrotask

--- packages/sycamore/src/lib.rs
+++ packages/sycamore/src/lib.rs
@@ -93,6 +93,7 @@ pub mod prelude {
     pub use crate::component::Children;
     pub use crate::flow::*;
     pub use crate::generic_node::{GenericNode, Html};
+    pub use crate::html::on_mount;
     pub use crate::noderef::{create_node_ref, NodeRef};
     pub use crate::reactive::*;
     pub use crate::view::View;
