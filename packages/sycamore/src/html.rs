5ddd5b2d5608540768b7cbe4ea847d3fd83117f9
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2022-05-15 21:38:30 -0700

    Fix compile errors when rebasing

--- packages/sycamore/src/html.rs
+++ packages/sycamore/src/html.rs
@@ -3,7 +3,6 @@
 //! _Documentation sources: <https://developer.mozilla.org/en-US/>_
 
 pub use sycamore_web::on_mount;
-use wasm_bindgen::prelude::*;
 
 use crate::builder::ElementBuilder;
 use crate::generic_node::SycamoreElement;
@@ -275,41 +274,6 @@ define_elements! {
     tspan {},
     r#use {},
     view {},
-}
-
-/// Queue up a callback to be executed when the component is mounted.
-///
-/// If not on `wasm32` target, does nothing.
-///
-/// # Potential Pitfalls
-///
-/// If called inside an async-component, the callback will be called after the next suspension
-/// point (when there is an `.await`).
-pub fn on_mount<'a>(cx: Scope<'a>, f: impl Fn() + 'a) {
-    if cfg!(target_arch = "wasm32") {
-        let scope_status = use_scope_status(cx);
-
-        #[wasm_bindgen]
-        extern "C" {
-            #[wasm_bindgen(js_name = "queueMicrotask")]
-            fn queue_microtask(f: &Closure<dyn Fn()>);
-        }
-
-        let f: Box<dyn Fn()> = Box::new(f);
-        // SAFETY: We do not access `f_extended` until we verify that the scope is still valid using
-        // `use_scope_status`.
-        let f_extended: Box<dyn Fn() + 'static> = unsafe { std::mem::transmute(f) };
-
-        let cb = move || {
-            if *scope_status.get() {
-                // Scope is still valid. We can safely execute the callback.
-                f_extended();
-            }
-        };
-        let boxed: Box<dyn Fn()> = Box::new(cb);
-        let closure = create_ref(cx, Closure::wrap(boxed));
-        queue_microtask(closure);
-    }
 }
 
 /// Props for [`NoHydrate`].
