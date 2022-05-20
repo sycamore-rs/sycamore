3484e0d6b57b19168ac9804ec7ab925394f97e32
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2022-05-19 20:28:51 -0700

    Remove useless box around create_effect_scoped

--- packages/sycamore-reactive/src/effect.rs
+++ packages/sycamore-reactive/src/effect.rs
@@ -153,7 +153,7 @@ pub fn create_effect_scoped<'a, F>(cx: Scope<'a>, mut f: F)
 where
     F: for<'child_lifetime> FnMut(BoundedScope<'child_lifetime, 'a>) + 'a,
 {
-    let mut disposer: Option<Box<ScopeDisposer<'a>>> = None;
+    let mut disposer: Option<ScopeDisposer<'a>> = None;
     create_effect(cx, move || {
         // We run the disposer inside the effect, after effect dependencies have been cleared.
         // This is to make sure that if the effect subscribes to its own signal, there is no
@@ -163,12 +163,11 @@ where
             unsafe { disposer.dispose() };
         }
         // Create a new nested scope and save the disposer.
-        let new_disposer: Option<Box<ScopeDisposer<'a>>> =
-            Some(Box::new(create_child_scope(cx, |cx| {
-                // SAFETY: f takes the same parameter as the argument to
-                // self.create_child_scope(_).
-                f(unsafe { std::mem::transmute(cx) });
-            })));
+        let new_disposer: Option<ScopeDisposer<'a>> = Some(create_child_scope(cx, |cx| {
+            // SAFETY: f takes the same parameter as the argument to
+            // self.create_child_scope(_).
+            f(unsafe { std::mem::transmute(cx) });
+        }));
         disposer = new_disposer;
     });
 }
