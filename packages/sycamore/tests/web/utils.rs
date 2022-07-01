3d3fbeaad19c79222e36b3b0e4dfaf27d6167a56
mc1098 <43726912+mc1098@users.noreply.github.com>
2022-07-01 16:58:17 +0100

    Add documentation for new functions and macro

--- packages/sycamore/tests/web/utils.rs
+++ packages/sycamore/tests/web/utils.rs
@@ -9,6 +9,14 @@ pub(crate) fn document() -> Document {
     window().document().unwrap()
 }
 
+/// Query the `Document` for the first `Element` that matches the selectors.
+///
+/// This is a test utility function only!
+///
+/// # Panics
+///
+/// Panics if the selectors string is invalid or if no element was found that
+/// matches the selectors
 pub(crate) fn query(selectors: &str) -> Element {
     document()
         .query_selector(selectors)
@@ -16,6 +24,17 @@ pub(crate) fn query(selectors: &str) -> Element {
         .expect("element to be found that matches the selectors")
 }
 
+/// Query the `Document` for the first `Element` that matches the selectors and
+/// then try to cast it into the generic type `T`.
+///
+/// This is a test utility function only!
+///
+/// # Panics
+///
+/// Panics if:
+/// - the selectors string is invalid
+/// - no element was found that matches the selectors
+/// - element found cannot be cast to the generic type `T` used
 pub(crate) fn query_into<T: AsRef<HtmlElement> + JsCast>(selectors: &str) -> T {
     // dyn_into -> unwrap to eagerly cause a panic if the query doesn't match
     // the generic T.
@@ -24,6 +43,8 @@ pub(crate) fn query_into<T: AsRef<HtmlElement> + JsCast>(selectors: &str) -> T {
         .expect("element found should be of the same type as used for the generic T")
 }
 
+/// Asserts that the text content of a `web_sys::Node` is equal to the
+/// right expression.
 macro_rules! assert_text_content {
     ($element: expr, $right: expr $(,)?) => {
         assert_eq!($element.text_content().unwrap(), $right);
