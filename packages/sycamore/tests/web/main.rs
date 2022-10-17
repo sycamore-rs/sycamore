8f360aa98dd3052aa547d2a72f1c190a971a9f40
Mark Wainwright <wainwrightml@gmail.com>
2022-10-17 08:44:11 +0100

    Created a test for noderef reactivity

--- packages/sycamore/tests/web/main.rs
+++ packages/sycamore/tests/web/main.rs
@@ -315,6 +315,40 @@ fn noderefs() {
 }
 
 #[wasm_bindgen_test]
+fn noderef_reactivity_test(){
+    create_scope_immediate(|cx| {
+        let counter : std::sync::Arc<_>  = std::cell::RefCell::new(0).into();
+        let counter1 = counter.clone();
+        let node_ref = create_node_ref(cx);
+        
+        let node1 = view! { cx,
+            div {
+                input(ref=node_ref)
+            }
+        };
+
+        create_effect(cx, move || {
+            node_ref.get::<DomNode>();
+            counter1.replace_with(|&mut x|x+ 1);
+        });
+
+        sycamore::render_to(|_| node1, &test_container());
+        assert_eq!(*counter.borrow(), 1);
+
+        let node2 = view! { cx,
+            div {
+                input(ref=node_ref)
+            }
+        };
+
+        sycamore::render_to(|_| node2, &test_container());
+
+        assert_eq!(*counter.borrow(), 2);
+
+    });
+}
+
+#[wasm_bindgen_test]
 fn fragments() {
     create_scope_immediate(|cx| {
         let node = view! { cx,
