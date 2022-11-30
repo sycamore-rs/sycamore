ce1286414c63d2326b8d5bab397e92cb91e3a4e2
Yerlan <yerlaser@gmail.com>
2022-12-01 04:04:06 +0600

    Update main.rs

--- examples/counter-with-tailwindcss/src/main.rs
+++ examples/counter-with-tailwindcss/src/main.rs
@@ -1,12 +1,9 @@
-use sycamore::prelude::*;
 use components::button::Button;
 use components::count::Count;
 use components::square::Square;
+use sycamore::prelude::*;
 
 mod components;
-
-// Add this to the index.html:
-//    <link data-trunk href="./tailwind.css" rel="css" />
 
 fn main() {
     sycamore::render(|cx| {
@@ -20,12 +17,12 @@ fn main() {
         });
         view! { cx,
             article(class="flex flex-col justify-center items-center") {
+                Count(value=state)
                 header() {
                     Button(updater=update, action=-1)
                     Button(updater=update, action=0)
                     Button(updater=update, action=1)
                 }
-                Count(value=state)
                 Square(value=state)
             }
         }
