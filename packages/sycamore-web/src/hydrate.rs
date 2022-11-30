4c91ecbfd3a56e8923e377e31d746c1c245bd1c7
Wei Tang <wei@that.world>
2022-11-30 14:50:18 +0100

    Fix clippy warning

--- packages/sycamore-web/src/hydrate.rs
+++ packages/sycamore-web/src/hydrate.rs
@@ -21,7 +21,7 @@ pub fn get_next_element() -> Option<Element> {
             .query_selector(&format!("[data-hk=\"{}.{}\"]", hk.0, hk.1))
             .unwrap()
             .map(|e| {
-                e.remove_attribute("data-hk");
+                e.remove_attribute("data-hk").unwrap();
                 e
             })
     } else {
