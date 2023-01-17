b26fc33b66ad94a726d8110b3b0dc48c83cccb5f
Valery Stepanov <etonomick@gmail.com>
2023-01-17 14:53:13 +0400

    Example of JS code next to Sycamore

---
+++ examples/js-snippets/src/main.rs
@@ -0,0 +1,36 @@
+use sycamore::prelude::*;
+use wasm_bindgen::prelude::wasm_bindgen;
+
+#[wasm_bindgen(module = "/js/functions.js")]
+extern "C" {
+    fn multiply(a: f64, b: f64) -> f64;
+}
+
+#[component]
+fn App<G: Html>(cx: Scope) -> View<G> {
+    let a = create_signal(cx, 0_f64);
+    let b = create_signal(cx, 0_f64);
+    let product = create_signal(cx, 0_f64);
+
+    create_effect(cx, || {
+        product.set(multiply(*a.get(), *b.get()));
+    });
+
+    view! {
+        cx,
+        input(type="number", bind:valueAsNumber=a)
+        span {"*"}
+        input(type="number", bind:valueAsNumber=b)
+        span {"="}
+        span {(*product.get())}
+    }
+}
+
+fn main() {
+    sycamore::render(|cx| {
+        view! {
+            cx,
+            App
+        }
+    });
+}
