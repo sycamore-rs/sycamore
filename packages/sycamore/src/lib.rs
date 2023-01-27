5f1ee457536e1b0b7bb35834732e288e55cb7e0b
Genna Wingert <wingertge@gmail.com>
2023-01-27 14:56:28 +0100

    Add unique ID generation hook (#565)
    Introduce the `create_unique_id` hook.

--- packages/sycamore/src/lib.rs
+++ packages/sycamore/src/lib.rs
@@ -51,7 +51,7 @@ pub mod utils;
 pub mod web;
 
 /* Re-export modules from sycamore-core */
-pub use sycamore_core::{component, generic_node, noderef, view};
+pub use sycamore_core::{component, generic_node, noderef, stable_id, view};
 /* Re-export of the sycamore-macro crate */
 pub use sycamore_macro::*;
 
@@ -87,6 +87,7 @@ pub mod prelude {
     pub use crate::generic_node::GenericNode;
     pub use crate::noderef::{create_node_ref, NodeRef};
     pub use crate::reactive::*;
+    pub use crate::stable_id::create_unique_id;
     pub use crate::view::View;
     #[cfg(feature = "web")]
     pub use crate::web::macros::{node, view};
