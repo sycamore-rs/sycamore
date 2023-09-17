8be69d6f953ff8e29412271227c9ad351f60290b
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2023-09-17 18:26:49 +0200

    Fix svg example

--- examples/svg/src/main.rs
+++ examples/svg/src/main.rs
@@ -6,14 +6,14 @@ use sycamore::prelude::*;
 
 #[component]
 fn App<G: Html>() -> View<G> {
-    let rotate = create_tweened_signal(0f64, Duration::from_millis(800), easing::quad_inout);
+    let mut rotate = create_tweened_signal(0f64, Duration::from_millis(800), easing::quad_inout);
 
     view! {
-        button(disabled=rotate.is_tweening(), on:click=|_| rotate.set(rotate.get() + 0.5)) { "Half rotate..." }
-        button(disabled=rotate.is_tweening(), on:click=|_| rotate.set(rotate.get() + 1.0)) { "Rotate!" }
-        button(disabled=rotate.is_tweening(), on:click=|_| rotate.set(rotate.get() + 2.0)) { "Rotate twice!!" }
-        button(disabled=rotate.is_tweening(), on:click=|_| rotate.set(rotate.get() + 3.0)) { "Rotate thrice!!!" }
-        svg(height="210", width="500", xmlns="http://www.w3.org/2000/svg") {
+        button(disabled=rotate.is_tweening(), on:click=move |_| rotate.set(rotate.get() + 0.5)) { "Half rotate..." }
+        button(disabled=rotate.is_tweening(), on:click=move |_| rotate.set(rotate.get() + 1.0)) { "Rotate!" }
+        button(disabled=rotate.is_tweening(), on:click=move |_| rotate.set(rotate.get() + 2.0)) { "Rotate twice!!" }
+        button(disabled=rotate.is_tweening(), on:click=move |_| rotate.set(rotate.get() + 3.0)) { "Rotate thrice!!!" }
+        svg(height="500", width="500", xmlns="http://www.w3.org/2000/svg") {
             rect(
                 x="100", y="100",
                 width="100", height="100",
