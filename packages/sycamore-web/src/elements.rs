eaadb2ac481c7bdf96f6979ba83919db7f3affcb
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2024-09-09 11:31:39 +0200

    Remove debug message (Oops!)

--- packages/sycamore-web/src/elements.rs
+++ packages/sycamore-web/src/elements.rs
@@ -1187,6 +1187,5 @@ pub trait GlobalAttributes: IntoHtmlNode + Sized {
 
 /// Helper function for setting a dynamic attribute.
 fn set_attribute(el: &mut HtmlNode, name: &'static str, value: impl AttributeValue) {
-    console_warn!("setting attribute `{name}`");
     value.set_self(el, name);
 }
