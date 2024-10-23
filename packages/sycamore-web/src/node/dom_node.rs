9f9869fc429231ec7949031f82d897f229a85e0e
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2024-10-23 21:20:56 +0100

    Fix compile error

--- packages/sycamore-web/src/node/dom_node.rs
+++ packages/sycamore-web/src/node/dom_node.rs
@@ -35,8 +35,8 @@ pub(crate) fn _create_dynamic_view<T: ViewHtmlNode, U: Into<View<T>> + 'static>(
     // effect to update its text value without ever creating more nodes.
     if TypeId::of::<U>() == TypeId::of::<String>() {
         create_effect_initial(move || {
-            let text: &mut Option<String> =
-                (&mut Some(f()) as &mut dyn Any).downcast_mut().unwrap();
+            let mut value = Some(f());
+            let text: &mut Option<String> = (&mut value as &mut dyn Any).downcast_mut().unwrap();
             let view = View::from_node(T::create_dynamic_text_node(text.take().unwrap().into()));
             debug_assert_eq!(
                 view.nodes.len(),
