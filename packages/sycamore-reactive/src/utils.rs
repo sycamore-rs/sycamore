7e582319ae2b1e2710d1af417e2f47fc87592f15
Luke <37006668+lukechu10@users.noreply.github.com>
2024-10-29 23:11:33 +0000

    Do not track callback in `on` function callback (#740)
    * Do not track callback in `on` util
    
    * Update docs

--- packages/sycamore-reactive/src/utils.rs
+++ packages/sycamore-reactive/src/utils.rs
@@ -53,12 +53,13 @@ impl_trackable_deps_for_tuple!(A, B, C, D, E, F, G, H, I, J);
 impl_trackable_deps_for_tuple!(A, B, C, D, E, F, G, H, I, J, K);
 impl_trackable_deps_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L);
 
-/// A helper function for making dependencies explicit.
+/// A helper function for making dependencies explicit. This will create a closure that tracks
+/// those and only those dependencies that are passed to it.
 ///
 /// # Params
 /// * `deps` - A list of signals/memos that are tracked. This can be a single signal or it can be a
 ///   tuple of signals.
-/// * `f` - The callback function.
+/// * `f` - The callback function. Any dependencies that are accessed in here will not be tracked.
 ///
 /// # Example
 /// ```
@@ -81,6 +82,32 @@ pub fn on<T>(
 ) -> impl FnMut() -> T + 'static {
     move || {
         deps._track();
-        f()
+        untrack(&mut f)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn on_untracks_callback() {
+        let _ = create_root(move || {
+            let trigger1 = create_signal(());
+            let trigger2 = create_signal(());
+
+            let mut counter = create_signal(0);
+            create_effect(on(trigger1, move || {
+                // This should not be tracked by the effect since it is inside the `on` callback.
+                trigger2.track();
+                counter += 1;
+            }));
+
+            assert_eq!(counter.get(), 1);
+            trigger1.set(());
+            assert_eq!(counter.get(), 2);
+            trigger2.set(());
+            assert_eq!(counter.get(), 2);
+        });
     }
 }
